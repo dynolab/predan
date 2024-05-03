f050d0fe4d510a19397a378dc9c1a9a0649c1a8d
ViKarp <91561214+ViKarp@users.noreply.github.com>
2024-05-03 17:52:33 +0300

    functions of compute permutations and count entropy

---
+++ bin/entropy.py
@@ -0,0 +1,139 @@
+import numpy as np
+import itertools as it
+
+import pandas as pd
+
+from permutation import permutation_series, count_permutation
+from markov_permutation import compute_transition_matrix
+
+
+def entropy(permutations: list) -> float:
+    """
+    Count permutation entropy of time series.
+    Parameters:
+        permutations(list): vector with a number of permutations of different types
+    Returns:
+        ent(float): permutation entropy
+    """
+    allperm = sum(permutations)
+    ent = 0
+    for i in range(len(permutations)):
+        probability = permutations[i] / allperm
+        if probability != 0:
+            ent -= probability * np.log2(probability)
+    return ent
+
+
+def sample_entropy(timeseries_data: list, window_size: int, r: float):
+    """
+    Count sample entropy of time series.
+    :param timeseries_data: time series
+    :param window_size: window for walking through a time series
+    :param r: tolerance interval
+    :return: sample entropy
+    """
+
+    def construct_templates(ts_data: list, m: int = 2):
+        num_windows = len(ts_data) - m + 1
+        return [ts_data[x:x + m] for x in range(0, num_windows)]
+
+    def is_match(template_1: list, template_2: list, r1: float):
+        return all([abs(x - y) < r1 for (x, y) in zip(template_1, template_2)])
+
+    def get_matches(templates: list, r1: float):
+        return len(list(filter(lambda x: is_match(x[0], x[1], r1), it.combinations(templates, 2))))
+
+    B = get_matches(construct_templates(timeseries_data, window_size), r)
+    A = get_matches(construct_templates(timeseries_data, window_size + 1), r)
+    return -np.log2(A / B)
+
+
+def entropy_rate(transition_matrix):
+    """
+    Compute the entropy rate  of a Markov chain of permutation and entropy of stationary distribution of permutation states.
+
+    Parameters:
+        transition_matrix (numpy.ndarray): Transition matrix of the Markov chain.
+
+    Returns:
+        float, float: Entropy rate and Markov permutation entropy.
+    """
+
+    # Compute stationary distribution
+    # TODO разобраться с нормальным вычислением. С помощью собственного вектора возникает проблема, когда 1 - собственное число кратности больше одного. С помощью решения системы уравнения stat_dist * P = stat_dist возникает проблема детерминантна, равного нулю. Пока что вычисляем приближенно за 50 шагов.
+    # eigenvalues, eigenvectors = np.linalg.eig(transition_probabilities.T)
+    # stationary_vector = np.real(eigenvectors[:, np.isclose(eigenvalues, 1)])
+
+    # n = len(transition_probabilities)
+    # A = np.append(transpose(transition_probabilities)-identity(n), [[1 for _ in range(n)]], axis=0)
+    # b = transpose(np.array([0 for _ in range(n)]+[1]))
+    # stationary_distribution = np.linalg.solve(transpose(A).dot(A), transpose(A).dot(b))
+
+    matrix_dot = transition_matrix.copy()
+    for _ in range(6):
+        matrix_dot = np.dot(matrix_dot, matrix_dot)
+    stationary_distribution = np.mean(matrix_dot, axis=0)
+
+    # Normalize the stationary distribution
+    stationary_distribution /= stationary_distribution.sum()
+
+    # Compute entropy rate of vectors stationary_distribution
+    ent_rate = -np.sum(np.nan_to_num(stationary_distribution * np.log2(stationary_distribution)))
+
+    # Compute entropy rate of transition matrix
+    tr_ent_rate = -np.sum(np.nan_to_num(
+        stationary_distribution * np.sum(np.nan_to_num(transition_matrix * np.log2(transition_matrix)),
+                                         axis=0)))
+
+    return ent_rate, tr_ent_rate
+
+
+def permutation_stationary_entropy(time_series: pd.Series, window: int) -> float:
+    """
+    Compute the entropy rate  of a Markov chain of permutation
+    :param time_series: Time series
+    :param window: window for walking through a time series
+    :return: entropy
+    """
+    permutation = permutation_series(time_series, window)
+    transition_matrix = compute_transition_matrix(permutation)
+
+    matrix_dot = transition_matrix.copy()
+    for _ in range(6):
+        matrix_dot = np.dot(matrix_dot, matrix_dot)
+
+    stationary_distribution = np.mean(matrix_dot, axis=0)
+
+    # Normalize the stationary distribution
+    stationary_distribution /= stationary_distribution.sum()
+
+    # Compute entropy rate of vectors stationary_distribution
+    ent_rate = -np.sum(np.nan_to_num(stationary_distribution * np.log2(stationary_distribution)))
+    return ent_rate
+
+
+def permutation_entropy_rate(time_series: pd.Series, window: int, num_transitions: int = 1) -> float:
+    permutation = permutation_series(time_series, window)
+    transition_matrix = compute_transition_matrix(permutation)
+
+    matrix_dot = transition_matrix.copy()
+    for _ in range(num_transitions):
+        matrix_dot = np.dot(matrix_dot, transition_matrix)
+    transition_probabilities = matrix_dot.copy()
+    for _ in range(5):
+        matrix_dot = np.dot(matrix_dot, matrix_dot)
+    stationary_distribution = np.mean(matrix_dot, axis=0)
+
+    # Normalize the stationary distribution
+    stationary_distribution /= stationary_distribution.sum()
+
+    # Compute entropy rate
+    tr_ent_rate = -np.sum(np.nan_to_num(
+        stationary_distribution * np.sum(np.nan_to_num(transition_probabilities * np.log2(transition_probabilities)),
+                                         axis=0)))
+    return tr_ent_rate
+
+
+def permutation_entropy(time_series: pd, window: int):
+    perm_ts = count_permutation(time_series, window)
+    return entropy(perm_ts)
--- bin/functions.py
+++ bin/functions.py
@@ -6,9 +6,12 @@ import itertools as it
 import scipy as sp
 from itertools import combinations
 
+from sklearn.model_selection import train_test_split
 
+
+# Several useful features
 def generate_ts(date_start: str = "29/06/2023 00:00:00", date_end: str = "30/07/2023 00:00:00", alpha: float = 0.6,
-                beta: float = 0.4, max_kf: float = 5, trend: float = 4, season: float = 0):
+                beta: float = 0.4, max_kf: float = 5, trend: float = 4, season: float = 0) -> pd.Series:
     """
     Generate time series with autocorrelation, trend and seasons dependency.
     Parameters:
@@ -20,7 +23,7 @@ def generate_ts(date_start: str = "29/06/2023 00:00:00", date_end: str = "30/07/
         trend(float): absolute additional value for introducing a trend component
         season(float): absolute addition value for elements that are one week apart
     Returns:
-        time_series(np.Series): generated time series
+        time_series: generated time series
     """
     time_series = pd.date_range(start=date_start, end=date_end, freq='H').to_series()
     time_series.index = time_series
@@ -39,7 +42,7 @@ def generate_ts(date_start: str = "29/06/2023 00:00:00", date_end: str = "30/07/
 
 
 def random_walk(date_start: str = "29/06/2023 00:00:00", date_end: str = "30/07/2023 00:00:00", noise: float = 10,
-                noise_dist: str = 'normal'):
+                noise_dist: str = 'normal') -> pd.Series:
     """
     Generate time series as random walk.
     Parameters:
@@ -48,7 +51,7 @@ def random_walk(date_start: str = "29/06/2023 00:00:00", date_end: str = "30/07/
         noise(float): noise component
         noise_dist(str): distribution of noise. 'normal': np.random.normal(0,noise). 'uniform': np.random.uniform(-noise, noise)
     Returns:
-        time_series(np.Series): generated time series
+        time_series: generated time series
     """
     time_series = pd.date_range(start=date_start, end=date_end, freq='H').to_series()
     time_series.index = time_series
@@ -60,67 +63,6 @@ def random_walk(date_start: str = "29/06/2023 00:00:00", date_end: str = "30/07/
         for i in time_series[1:]:
             time_series[i] = time_series[i - dt.timedelta(1 / 24)] + np.random.uniform(-noise, noise)
     return time_series
-
-
-def count_permutation(ts: pd.Series, window: int):
-    """
-    Count permutation in time series
-    Parameters:
-        ts(pd.Series): time series
-        window(int): window for walking through a time series for which the type of permutation is located
-    Returns:
-        count_p(list): vector with a number of permutations of different types. The length of the vector is equal to the factorial of the window value
-    """
-    r = list(range(window))
-    permutation_list = list(it.permutations(r))
-    count_p = [0] * len(permutation_list)
-    for i in range(ts.size - window + 1):
-        slice_df = ts.copy().iloc[i:i + window]
-        sort_slice = slice_df.copy().sort_values()
-        for j in range(window):
-            slice_df[slice_df[slice_df == sort_slice.iloc[j]].first_valid_index()] = j
-        count_p[permutation_list.index(tuple(slice_df.tolist()))] += 1
-    return count_p
-
-
-def entropy(permutations: list):
-    """
-    Count permutation entropy of time series.
-    Parameters:
-        permutations(list): vector with a number of permutations of different types
-    Returns:
-        ent(float): permutation entropy
-    """
-    allperm = sum(permutations)
-    ent = 0
-    for i in range(len(permutations)):
-        probability = permutations[i] / allperm
-        if probability != 0:
-            ent -= probability * np.log2(probability)
-    return ent
-
-
-def sample_entropy(timeseries_data: list, window_size: int, r: float):
-    """
-    Count sample entropy of time series.
-    :param timeseries_data: time series
-    :param window_size: window for walking through a time series
-    :param r: tolerance interval
-    :return: sample entropy
-    """
-    def construct_templates(ts_data: list, m: int = 2):
-        num_windows = len(ts_data) - m + 1
-        return [ts_data[x:x + m] for x in range(0, num_windows)]
-
-    def is_match(template_1: list, template_2: list, r1: float):
-        return all([abs(x - y) < r1 for (x, y) in zip(template_1, template_2)])
-
-    def get_matches(templates: list, r1: float):
-        return len(list(filter(lambda x: is_match(x[0], x[1], r1), it.combinations(templates, 2))))
-
-    B = get_matches(construct_templates(timeseries_data, window_size), r)
-    A = get_matches(construct_templates(timeseries_data, window_size + 1), r)
-    return -np.log2(A / B)
 
 
 def equals_test(n1: int, n2: int, n: int):
@@ -170,3 +112,183 @@ def multiply_equals_test(permutations: list, alpha: float = 0.05, method: str = 
                                          sum(permutations))
         p_list.append(p_value)
     return sm.stats.multipletests(p_list, alpha, method)
+
+
+def big_split(sym_test: str, columns: list or None = None, test_size: float = 0.05, random_state: int = 42):
+    """
+    Splits all time series into train and test with sym_test and without
+    :param columns: list of time series features
+    :param test_size: size of test dataframe
+    :param random_state: set randomizer seed
+    :return:
+    """
+    dfM = pd.read_csv("../src/feature/month.csv", index_col=0)
+    dfY = pd.read_csv("../src/feature/year.csv", index_col=0)
+    dfQ = pd.read_csv("../src/feature/quater.csv", index_col=0)
+    dfD = pd.read_csv("../src/feature/day.csv", index_col=0)
+    dfH = pd.read_csv("../src/feature/hour.csv", index_col=0)
+    dfW = pd.read_csv("../src/feature/week.csv", index_col=0)
+    df_res = pd.read_csv("all_results.csv", index_col=0)
+    assert sym_test in df_res.columns, "sym_test not found. You can choose one from ['sym_test', 'sym_test1', 'sym_test2', 'sym_pv', 'sym_pv1'"
+
+    def split(df, format):
+        y = df_res.loc[df_res.index.str.startswith(format)]
+        y = y.reindex(df.index)
+        if columns is None:
+            X_train, X_test, y_train, y_test = train_test_split(df, y['smape'], test_size=test_size,
+                                                                random_state=random_state)
+        else:
+            X_train, X_test, y_train, y_test = train_test_split(df[columns], y['smape'], test_size=test_size,
+                                                                random_state=random_state)
+        df_with_test = df.copy()
+        df_with_test[sym_test] = y[sym_test]
+        if columns is None:
+            X_trainT, X_testT, y_trainT, y_testT = train_test_split(df_with_test, y['smape'], test_size=test_size,
+                                                                    random_state=random_state)
+        else:
+            X_trainT, X_testT, y_trainT, y_testT = train_test_split(df_with_test[columns + [sym_test]], y['smape'],
+                                                                    test_size=test_size, random_state=random_state)
+        return ([X_train, X_test, y_train, y_test], [X_trainT, X_testT, y_trainT, y_testT])
+
+    df_splitY, df_splitYT = split(dfY, 'Y')
+    df_splitQ, df_splitQT = split(dfQ, 'Q')
+    df_splitM, df_splitMT = split(dfM, 'M')
+    df_splitW, df_splitWT = split(dfW, 'W')
+    df_splitD, df_splitDT = split(dfD, 'D')
+    df_splitH, df_splitHT = split(dfH, 'H')
+    X_train = pd.concat([df_splitY[0], df_splitQ[0], df_splitM[0], df_splitW[0], df_splitD[0], df_splitH[0]])
+    X_test = pd.concat([df_splitY[1], df_splitQ[1], df_splitM[1], df_splitW[1], df_splitD[1], df_splitH[1]])
+    y_train = pd.concat([df_splitY[2], df_splitQ[2], df_splitM[2], df_splitW[2], df_splitD[2], df_splitH[2]])
+    y_test = pd.concat([df_splitY[3], df_splitQ[3], df_splitM[3], df_splitW[3], df_splitD[3], df_splitH[3]])
+    X_trainT = pd.concat([df_splitYT[0], df_splitQT[0], df_splitMT[0], df_splitWT[0], df_splitDT[0], df_splitHT[0]])
+    X_testT = pd.concat([df_splitYT[1], df_splitQT[1], df_splitMT[1], df_splitWT[1], df_splitDT[1], df_splitHT[1]])
+    y_trainT = pd.concat([df_splitYT[2], df_splitQT[2], df_splitMT[2], df_splitWT[2], df_splitDT[2], df_splitHT[2]])
+    y_testT = pd.concat([df_splitYT[3], df_splitQT[3], df_splitMT[3], df_splitWT[3], df_splitDT[3], df_splitHT[3]])
+    return X_train, X_test, y_train, y_test, X_trainT, X_testT, y_trainT, y_testT
+
+
+def generate_arma(p, q, n):
+    """
+    Генерация авторегрессионного скользящего временного ряда (ARMA(p,q)).
+
+    Параметры:
+    - p: int, порядок авторегрессии
+    - q: int, порядок скользящего среднего
+    - n: int, количество точек в ряду
+
+    Возвращает:
+    - временной ряд
+    """
+    arparams = np.random.uniform(low=-1, high=1, size=p)
+    maparams = np.random.uniform(low=-1, high=1, size=q)
+    ar = np.r_[1, -arparams]  # add zero-lag and negate
+    ma = np.r_[1, maparams]  # add zero-lag
+    arma_process = sm.tsa.ArmaProcess(ar, ma)
+    while np.min(np.abs(arma_process.arroots)) < 1:
+        arparams = np.random.uniform(low=-1, high=1, size=p)
+        maparams = np.random.uniform(low=-1, high=1, size=q)
+        ar = np.r_[1, -arparams]  # add zero-lag and negate
+        ma = np.r_[1, maparams]  # add zero-lag
+        arma_process = sm.tsa.ArmaProcess(ar, ma)
+    time_series = arma_process.generate_sample(n)
+    return time_series
+
+
+def generate_rw(n):
+    """
+    Генерация случайного блуждания.
+
+    Параметры:
+    - n: int, количество точек в ряду
+
+    Возвращает:
+    - временной ряд
+    """
+    # Генерация случайного шума
+    noise = np.random.randn(n)
+
+    # Создание временного ряда из случайного шума
+    time_series = np.cumsum(noise)
+
+    return time_series
+
+
+def generate_multifractal(n, H):
+    """
+    Генерация мультифрактального временного ряда.
+
+    Параметры:
+    - n: int, количество точек в ряду
+    - H: float, параметр Хёрста, определяющий степень мультифрактальности
+
+    Возвращает:
+    - временной ряд
+    """
+    # Генерация случайного шума
+    noise = np.random.randn(n)
+
+    # Создание временного ряда из случайного шума
+    time_series = np.cumsum(noise)
+
+    # Применение мультифрактальности к временному ряду
+    time_series = time_series / np.std(time_series)
+    time_series = np.power(np.abs(time_series), H) * np.sign(time_series)
+
+    return time_series
+
+
+def generate_trend_series(length=250, trend_koef=2, ampl=10, seasonality_period=7, seasonality_aplitude=1,
+                          sigma=1):
+    """
+    Generate sin series with trend? seasonality and noise.
+    :param length: length of the series.
+    :param freq: frequency of the sin.
+    :param ampl: amplitude of the sin.
+    :param seasonality_period: The period of the seasonality (e.g., 12 for monthly data).
+    :param seasonality_aplitude: The amplitude of the seasonality
+    :param sigma: The standard deviation of the error term.
+    :return: A NumPy array representing the sin series with trend, seasonality, and error.
+    """
+
+    def add_components(stationary_series, trend_type="linear", seasonality_period=7, seasonality_aplitude=1,
+                       sigma=1, trend_koef=1):
+        """
+      This function adds trend, seasonality, and error to a stationary series.
+
+      Args:
+        stationary_series: A NumPy array representing the stationary series.
+        trend_type: The type of trend to add. Can be "linear" or "nonlinear".
+        seasonality_period: The period of the seasonality (e.g., 12 for monthly data).
+        sigma: The standard deviation of the error term.
+        seasonality_aplitude: The amplitude of the seasonality
+
+      Returns:
+          A NumPy array representing the series with trend, seasonality, and error.
+      """
+
+        # Add trend
+        if trend_type == "linear":
+            trend = np.arange(len(stationary_series))
+        else:
+            # TODO Add a more complex nonlinear trend if desired
+            # ...
+            pass
+
+        # Add seasonality
+        seasonality = np.zeros(len(stationary_series))
+        for i in range(len(stationary_series)):
+            seasonality[i] = seasonality_aplitude * np.sin(2 * np.pi * i / seasonality_period)
+
+        # Add error
+        error = np.random.normal(loc=0, scale=sigma, size=len(stationary_series))
+
+        # Combine all components
+        series_with_trend_seasonality_error = stationary_series + trend * trend_koef + seasonality + error
+
+        return series_with_trend_seasonality_error
+
+    x = np.linspace(0, 2 * np.pi, length)
+    series = ampl * np.sin(x)
+    new_series = add_components(series, seasonality_period=seasonality_period,
+                                seasonality_aplitude=seasonality_aplitude, sigma=sigma, trend_koef=trend_koef)
+    return new_series
